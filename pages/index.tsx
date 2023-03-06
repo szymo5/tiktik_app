c7cc04ee29b3f329513bb52f91386a21de17d0ce
unknown <szymon.wysoka206@gmail.com>
2023-03-06 22:46:41 +0100

    sanity config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,28 @@
-import { Inter } from 'next/font/google'
-import { NextPage } from 'next'
+import axios from 'axios';
+import { Video } from '@/types';
 
-const inter = Inter({ subsets: ['latin'] })
+interface IProps {
+  videos: Video[];
+}
 
-const Home: NextPage = () => {
+const Home = ({videos}: IProps) => {
+  console.log(videos)
+
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
   )
+}
+
+export const getServerSideProps = async () => {
+  const {data} = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: {
+      videos: data
+    }
+  }
 }
 
 export default Home;
