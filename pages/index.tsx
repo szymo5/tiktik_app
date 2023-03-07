cf34e96d5ebdb785cf8f59f32c623d7bdce64573
unknown <szymon.wysoka206@gmail.com>
2023-03-07 23:10:13 +0100

    VideoCard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { Video } from '@/types';
+import VideoCard from '@/components/VideoCard';
+import NoResults from '@/components/NoResults';
 
 interface IProps {
   videos: Video[];
@@ -9,9 +11,15 @@ const Home = ({videos}: IProps) => {
   console.log(videos)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video: Video) =>(
+          <VideoCard post={video} key={video._id}/>
+        ))
+      ) : (
+        <NoResults text={'No Videos'} />
+      )}
+    </div>
   )
 }
 
