e0f47dc8154dd635fa47588461f2b69beeb00cd1
unknown <szymon.wysoka206@gmail.com>
2023-03-08 14:48:14 +0100

    VideoCard component

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -14,30 +14,102 @@ interface IProps {
 }
 
 const VideoCard: NextPage<IProps> = ({ post }) => { // is the same post: IProps
-  return (
-    <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
-        <div>
-            <div className='flex gap-3 p-2 cursor-pointer font-semibold rounded'>
-                <div className='md:w-16 md:h-16 w-10 h-10'>
-                    <Link href="/">
-                        <>
-                            <Image
-                                width={62}
-                                height={62}
-                                className='rounded-full'
-                                src={post.postedBy.image}
-                                alt="profile phoot"
-                                layout='responsive'
-                            >
+    const [isHover, setisHover] = useState(false);
+    const [playing, setPlaying] = useState(false);
+    const [isVideoMuted, setisVideoMuted] = useState(false);
 
-                            </Image>
-                        </>
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    const onVideoPress = () => {
+        if(playing) {
+            videoRef?.current?.pause();
+            setPlaying(false);
+        } else {
+            videoRef?.current?.play();
+            setPlaying(true);
+        }
+    }
+
+    return (
+        <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
+            <div>
+                <div className='flex gap-3 p-2 cursor-pointer font-semibold rounded'>
+                    <div className='md:w-16 md:h-16 w-10 h-10'>
+                        <Link href="/">
+                            <>
+                                <Image
+                                    width={62}
+                                    height={62}
+                                    className='rounded-full'
+                                    src={post.postedBy.image}
+                                    alt="profile phoot"
+                                    layout='responsive'
+                                />
+                            </>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link href="/">
+                            <div className='flex items-center gap-2'>
+                                <p className='flex gap-2 items-center md:text-md font-bold text-primary'>
+                                    {post.postedBy.userName}{` `}
+                                    <GoVerified className='text-blue-400 text-md'/>
+                                </p>
+                                <p className='capitalize font-medium text-xs text-gray-500 hidden md:block'>
+                                    {post.postedBy.userName}
+                                </p>
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+
+            <div className='lg:ml-20 flex gap-4 relative'>
+                <div 
+                    onMouseEnter={() => setisHover(true)}
+                    onMouseLeave={() => setisHover(false)}
+                    className='rounded-3xl'
+                >
+                    <Link href="/">
+                        <video 
+                            ref={videoRef}
+                            src={post.video.asset.url}
+                            loop
+                            className='lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-200'
+                        >
+
+                        </video>
                     </Link>
+                    {1 && (
+                        <div className='absolute bottom-6 cursor-pointer left-8 md:left-14 lg:left-0 flex gap-10 lg:justify-between w-[100px] md:w-[50px] p-3'>
+                            {
+                                playing ? (
+                                    <button onClick={onVideoPress}>
+                                        <BsFillPauseFill className='text-black text-2xl lg:text-4xl'/>
+                                    </button>
+                                ) : (
+                                    <button onClick={onVideoPress}>
+                                        <BsFillPlayFill className='text-black text-2xl lg:text-4xl'/>
+                                    </button>
+                                )
+                            }
+                            {
+                                isVideoMuted ? (
+                                    <button onClick={() => setisVideoMuted(false)}>
+                                        <HiVolumeOff className='text-black text-2xl lg:text-4xl'/>
+                                    </button>
+                                ) : (
+                                    <button onClick={() => setisVideoMuted(true)}>
+                                        <HiVolumeUp className='text-black text-2xl lg:text-4xl'/>
+                                    </button>
+                                )
+                            }
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default VideoCard
