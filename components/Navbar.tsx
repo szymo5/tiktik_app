49a6f9c04f1d3551094824ac6e52044958013a05
unknown <szymon.wysoka206@gmail.com>
2023-03-09 19:18:30 +0100

    add google login

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import Image from 'next/legacy/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
+// import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import {AiOutlineLogout} from 'react-icons/ai';
+import {GoogleLogin, googleLogout} from '@react-oauth/google';
 import {BiSearch} from 'react-icons/bi';
 import {IoMdAdd} from 'react-icons/io';
 
 import Logo from '../utils/tiktik-logo.png';
 
 const Navbar = () => {
+  const user = false;
+
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
         <Link href='/'>
@@ -22,6 +25,19 @@ const Navbar = () => {
                 />
             </div>
         </Link>
+        <div>
+          Search
+        </div>
+        <div>
+          {user ? (
+            <div>Logged In</div>
+          ) : (
+            <GoogleLogin 
+              onSuccess={(response) => console.log(response)}
+              onError={() => console.log('error')}
+            />
+          )}
+        </div>
     </div>
   )
 }
