5bb9434df5335919f7a7875edc93c26c39225679
unknown <szymon.wysoka206@gmail.com>
2023-03-15 23:21:29 +0100

    detail post page and fixing

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react'
+import { useRouter } from 'next/router'
+import Image from 'next/image'
+import Link from 'next/link'
+import { GoVerified } from 'react-icons/go'
+import { MdOutlineCancel } from 'react-icons/md'
+import { BsFillPlayFill } from 'react-icons/bs'
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi'
+import axios from 'axios'
 
-const Detail = () => {
+import { BASE_URL } from '@/utils'
+import { Video } from '@/types'
+
+interface IProps {
+  postDetails: Video,
+}
+
+const Detail = ({ postDetails }: IProps) => {
   return (
     <div>Detail</div>
   )
+}
+
+export const getServerSideProps = async ({params: {id}}: {params: {id: string}}) => {
+  const {data} = await axios.get(`${BASE_URL}/api/post/${id}`)
+
+  return {
+    props: {postDetails: data}
+  }
 }
 
 export default Detail
