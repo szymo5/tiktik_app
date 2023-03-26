bf8f155eaf5cc208df224ddc70e08b0eca5e6c37
unknown <szymon.wysoka206@gmail.com>
2023-03-26 22:50:39 +0200

    video muted functionality

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -21,16 +21,23 @@ const Detail = ({ postDetails }: IProps) => {
   const [isVideoMuted, setIsVideoMuted] = useState(false)
 
   const videoRef = useRef<HTMLVideoElement>(null);
+  const router = useRouter();
 
   const onVideoClick = () => {
     if(playing) {
       videoRef?.current?.pause();
       setPlaying(false);
-  } else {
-      videoRef?.current?.play();
-      setPlaying(true);
+    } else {
+        videoRef?.current?.play();
+        setPlaying(true);
+    }
   }
-  }
+
+  useEffect(() => {
+    if(post && videoRef?.current) {
+        videoRef.current.muted = isVideoMuted;
+    }
+  }, [post, isVideoMuted])
 
   if(!post) return null;
 
@@ -38,7 +45,7 @@ const Detail = ({ postDetails }: IProps) => {
     <div className='flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap'>
       <div className='relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-black'>
         <div className='absolute top-6 left-2 lg:left-6 flex gap-6 z-50'>
-          <p>
+          <p className='cursor-pointer' onClick={() => router.back()}>
             <MdOutlineCancel className='text-white text-[35px]'/>
           </p>
         </div>
@@ -75,6 +82,7 @@ const Detail = ({ postDetails }: IProps) => {
         }
         </div>
       </div>
+      
     </div>
   )
 }
