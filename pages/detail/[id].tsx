30af5f4b74926e16d72d2d75b8d0bbd1b4032c24
unknown <szymon.wysoka206@gmail.com>
2023-04-17 21:48:12 +0200

    Comments implementation

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -26,6 +26,8 @@ const Detail = ({ postDetails }: IProps) => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const router = useRouter();
   const {userProfile}: any = useAuthStore();
+  const [comment, setComment] = useState('');
+  const [isPostingComment, setIsPostingComment] = useState(false)
 
   const onVideoClick = () => {
     if (playing) {
@@ -52,6 +54,19 @@ const Detail = ({ postDetails }: IProps) => {
       })
 
       setPost({...post, likes: data.likes})
+    }
+  }
+
+  const addComment = async (e:any) => {
+    e.preventDefault();
+
+    if(userProfile && comment) {
+      setIsPostingComment(true);
+
+      const response = await axios.put(`${BASE_URL}/api/post/${post._id}`, {
+        userId: userProfile._id,
+        comment
+      });
     }
   }
 
@@ -139,7 +154,13 @@ const Detail = ({ postDetails }: IProps) => {
               />
             )}
           </div>
-          <Comments />
+          <Comments 
+            comment={comment}
+            setComment={setComment}
+            addComment={addComment}
+            comments={post.comments}
+            isPostingComment={isPostingComment}
+          />
         </div>
       </div>
     </div>
