c83ec6dd8b7a51d6ac0fbfb41730f0eab9eedf03
unknown <szymon.wysoka206@gmail.com>
2023-04-21 16:16:02 +0200

    Comments implementation

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Dispatch, SetStateAction} from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { GoVerified } from 'react-icons/go'
@@ -9,8 +9,8 @@ import NoResults from './NoResults'
 interface IProps {
   isPostingComment: Boolean,
   comment: string,
-  setComment: () => void;
-  addComment: () => void;
+  setComment: Dispatch<SetStateAction<string>>
+  addComment: (e: React.FormEvent) => void;
   comments: IComment[];
 }
 
@@ -36,10 +36,10 @@ const Comments = ({comment, setComment, addComment, comments, isPostingComment}:
 
       {userProfile && (
         <div className='absolute bottom-0 left-0 pb-6 px-2 md:px-10'>
-            <form onSubmit={() => {}} className='flex gap-4'>
+            <form onSubmit={addComment} className='flex gap-4'>
               <input 
-                value=""
-                onChange={() => {}}
+                value={comment}
+                onChange={(e) => {setComment(e.target.value)}}
                 placeholder='Add comment...'
                 className='bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg'
               />
